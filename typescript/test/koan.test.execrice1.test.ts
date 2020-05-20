c18f0bec1b32eceab7d5f109247a84f7ce4cc7cf
mathieu cans <mathieucans@gmail.com>
2020-05-20 09:19:13 +0200

    solution

---
+++ typescript/test/koan.test.execrice1.test.ts
@@ -0,0 +1,21 @@
+/*
+    Learn to use short cut to create and execute test.
+        - use ⌘⏎ to complete code statement
+        - use ^↑ to select first test
+        - use ⌘D to duplicate first test
+        - use ⌘F to find all "first" reference and the navigate with ⌘G to name second and third tests
+        - execute first test by using ^⇧R
+        - re-execute last test execution by using ^R
+        - execute all test by placing cursor between two test and using ^⇧R
+ */
+describe('first koan', () => {
+    test('first test', () => {
+
+    });
+    test('first test', () => {
+
+    });
+    test('first test', () => {
+
+    });
+});
