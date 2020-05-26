da42aac18c41c8311208314a661611638203ab91
mathieu cans <mathieucans@gmail.com>
2020-05-26 09:10:49 +0200

    Add delete line step

--- typescript/test/kaon.basics.nagigate.selection.deletion.test.ts
+++ typescript/test/kaon.basics.nagigate.selection.deletion.test.ts
@@ -9,11 +9,12 @@ describe('navigate, selection and deletion', () => {
     test('deletion', () => {
         // Delete all unused spaces by using delete tow Word End ⌥⌦ / Ctrl+Delete
         expect('                             to much spaces kill spaces').not.toContain('    ');
+    });
 
-        /// Delete this line by using ⌘⌫
-
-
-
+    test('Delete the failing line', () => {
+        let deleteUnusedLine = 'This test should pass';
+        deleteUnusedLine += '*** delete me by using ⌘⌫ ****';
+        expect(deleteUnusedLine).toEqual('This test should pass');
     });
 
     test('Delete this test and navigate to begin block by using ⌘⌥[', () => {
