3e5dcf9c48f55d40e246e0d4f995d2477dbb16b2
mathieu cans <mathieucans@gmail.com>
2020-05-27 17:55:42 +0200

    Add extract interface

--- typescript/test/koan.2.code.generation.test.ts
+++ typescript/test/koan.2.code.generation.test.ts
@@ -1,3 +1,5 @@
+import {French, SayHello} from "../src/SayHello";
+
 describe('Generate code by using your IDE', () => {
     test('use ⌥⏎ to generate class and method ', () => {
         let buildResult = false;
@@ -5,5 +7,14 @@ describe('Generate code by using your IDE', () => {
         //buildResult = new RobotCode().build();
 
         expect(buildResult).toEqual(true);
+    });
+
+    test('extract interface Language of French to make this test pass', () => {
+        // const language = English();
+        let sayHello = new SayHello(new French());
+
+        let response = sayHello.hello();
+
+        expect(response).toEqual('Hello David!');
     });
 });
