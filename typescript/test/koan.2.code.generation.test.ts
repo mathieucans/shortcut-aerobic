67f5d1e88999b03873d2b637c33756740967c39e
mathieu cans <mathieucans@gmail.com>
2020-05-27 14:00:06 +0200

    Add code generation step

---
+++ typescript/test/koan.2.code.generation.test.ts
@@ -0,0 +1,9 @@
+describe('Generate code by using your IDE', () => {
+    test('use ⌥⏎ to generate class and method ', () => {
+        let buildResult = false;
+
+        //buildResult = new RobotCode().build();
+
+        expect(buildResult).toEqual(true);
+    });
+});
