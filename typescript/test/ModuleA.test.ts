1b7f71940e11a617f56d5dbaedc679c8137b6c82
mathieu cans <mathieucans@gmail.com>
2020-05-27 13:48:37 +0200

    Remove module A test

--- typescript/test/ModuleA.test.ts
+++
@@ -1,9 +0,0 @@
-import ModuleA from "../src/moduleA";
-
-describe('My test', () => {
-    test('first test', () => {
-        const test = new ModuleA().test();
-
-        expect(test).toEqual("youpii");
-    });
-});
