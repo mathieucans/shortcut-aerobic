f3a7986069ecec8f671757ba43da118f5f4e30d7
mathieu cans <mathieucans@gmail.com>
2020-05-27 08:34:24 +0200

    Add navigation through F2

--- typescript/test/koan.0.basic.start.test.ts
+++ typescript/test/koan.0.basic.start.test.ts
@@ -6,4 +6,8 @@ describe('Get started :  navigate to to next failing test', () => {
     test('use ⌘4 and arrow to select the first failing test', () => {
         expect('then press ⌘⏎, ␛ to navigate to source').toEqual('then make this test pass');
     });
+
+    test('you cans also navigate to next failing test by using F2', () => {
+        expect(`it's quite easier isn't it ?`).toEqual('then make this test pass');
+    });
 });
