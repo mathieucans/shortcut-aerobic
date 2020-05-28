29245cdcfcfdf6dcd781f8cfbcba2810448f1062
mathieu cans <mathieucans@gmail.com>
2020-05-28 09:16:56 +0200

    enhance generate code

--- typescript/test/koan.2.code.generation.test.ts
+++ typescript/test/koan.2.code.generation.test.ts
@@ -2,14 +2,16 @@ import {French, SayHello} from "../src/SayHello";
 
 describe('Generate code by using your IDE', () => {
     test('Uncomment and generate class and method by unsing ⌥⏎ / Alt+Enter', () => {
+        // TIPS : you can navigate through error code by using F2
         let buildResult = "code";
 
-        //buildResult = new RobotCode().concat(buildeResult);
+        //buildResult = new RobotCode().repeat(buildResult);
 
-        expect(buildResult).toEqual("code code");
+        expect(buildResult).toEqual("codecode");
     });
 
     test('extract interface Language of French by using ^T / Ctrl+Shift+Alt+T', () => {
+        // TIPS : Navigate to French implementation by using ⌘B / Ctrl+B
         // const language = English();
         let sayHello = new SayHello(new French());
 
