df58da9a3c9bc4a5c6c1d4ff2a80574c155e0d4f
Mathieu Cans <mathieu.cans@orange.com>
2020-08-20 09:03:15 +0200

    navigation koans with dotnet resharper

--- dotnetcore/GetStarted_run_test_and_navigate_to_next_failing_test.cs
+++ dotnetcore/GetStarted_run_test_and_navigate_to_next_failing_test.cs
@@ -5,26 +5,33 @@ namespace dotnetcore
 {
     public class GetStarted_run_test_and_navigate_to_next_failing_test
     {
+
         [Fact]
-        public void Run_all_test_of_this_file()
+        public void Koan01_run_all_of_the_solution()
         {
-            Assert.Equal("by using ^⇧R with cursor beetween two test", "done");
+            Assert.Equal("by using Ctrl+U,L", "done");
+        } 
+
+        [Fact]
+        public void Koan02_run_all_test_of_this_file()
+        {
+            Assert.Equal("by using Ctrl+U,R with cursor beetween two test", "done");
         } 
         
         [Fact]
-        public void Run_last_test_execution()
+        public void Koan03_Run_last_test_execution()
         {
-            Assert.Equal("by using ⌘;,Y", "done");
+            Assert.Equal("by using Ctrl+U,Y", "done");
         }
 
         [Fact]
-        public void Navigate_to_next_or_previous_test()
+        public void Koan04_Navigate_to_next_or_previous_test()
         {
-            Assert.Equal("by using ⌘↑ or ⌘↓", "done");            
+            Assert.Equal("by using Alt+↑ or Alt+↓", "done");            
         }
         
         [Fact]
-        public void Run_this_test_only()
+        public void Koan05_Run_this_test_only()
         {
             Assert.Equal("by using ^⇧R with cursor inside this test", "done");
         }
