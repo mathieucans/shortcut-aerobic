b3985604f4a627476cfe333de66d307594d05998
mathieu cans <mathieucans@gmail.com>
2020-09-22 12:39:47 +0200

    first test is red

---
+++ kotlin/src/test/kotlin/Koan0BasicStartTests.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Koan0BasicStartTests {
+
+    @Test
+    fun firstTest() {
+        assertThat("This test").isEqualTo("red")
+    }
+}
