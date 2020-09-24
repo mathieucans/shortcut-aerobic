9c980319277f74d6c31a1a87a23406247f122cee
mathieu cans <mathieucans@gmail.com>
2020-09-24 14:05:39 +0200

    Koan 0 all test

--- kotlin/src/test/kotlin/Koan0BasicStartTests.kt
+++ kotlin/src/test/kotlin/Koan0BasicStartTests.kt
@@ -4,7 +4,34 @@ import org.junit.jupiter.api.Test
 class `Koan 0 - Basics - Get started - navigate to to next failing test` {
 
     @Test
-    fun `first of all, run all test of this file with cursor on top of the file by using ^⇧R | Ctrl+Shift+F10`() {
+    fun `01 - first of all, run all test of this file with cursor on top of the file by using ^⇧R | Ctrl+Shift+F10`() {
         assertThat("Then change this to make this test pass").isEqualTo(true)
     }
+
+    @Test
+    fun `02 - make this test pass and rerun last test execution by using ^R  |  Shift + F10`() {
+        assertThat(true).isFalse()
+    }
+
+    @Test
+    fun `03 - you cans also navigate to next failing test by using F2`(){
+        assertThat("it's quite easier isn't it ?").isEqualTo("then make this test pass")
+    }
+
+    @Test
+    fun `04 - make this test pass and rerun only this test by using ^⇧R | Crtl+Shift+F10 with cursor inside this test scope`() {
+        assertThat(true).isFalse()
+    }
+
+    @Test
+    fun `05 - make this test pass and rerun all test by using ^⇧R | Crtl+Shift+F10 with cursor between two tests`() {
+        assertThat(true).isFalse()
+    }
+
+    @Test
+    fun `06 - you can open next koan by Navigate to file by using ⌘⇧O | Ctrl+Shift+N`() {
+        assertThat(true).isFalse()
+    }
+
+
 }
