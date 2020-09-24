71b0db84341d15d723018e8da04679d52039121f
mathieu cans <mathieucans@gmail.com>
2020-09-24 14:10:36 +0200

    Create koan 1 file test

---
+++ kotlin/src/test/kotlin/Koan1BasicsNavigateSelectionSeletion.kt
@@ -0,0 +1,53 @@
+class `Koan 0 navigate, selection and deletion` {
+/*    /// Navigate to end of file by using ⌘A,→  / Ctrl+A, →
+    test('This test pass, use ⌘⌥] to navigate to end block and delete last failing test', () => {
+        expect(new ModuleA().test()).toEqual('youpii');
+    });
+
+    test('Deletion to next word', () => {
+        // Delete all unused spaces by using delete to Word End ⌥⌦ / Ctrl+Delete
+        expect('                             to much spaces kill spaces').not.toContain('    ');
+    });
+
+    test('Deletion to word start word', () => {
+        // Delete MouseForEver by using delete to Word Start ⌥⌫ / Ctrl+Backspace
+        expect('delete the last straw!MouseForEver').not.toContain('MouseForEver');
+    });
+
+    test('Delete the failing line', () => {
+        let deleteUnusedLine = 'This test should pass';
+        deleteUnusedLine += '*** delete me by using ⌘⌫ / Ctrl+Y ****';
+        expect(deleteUnusedLine).toEqual('This test should pass');
+    });
+
+    test('Extend selection by using ⌥↑ / Ctrl+W', () => {
+        expect(`***** SELECT ME WITH EXTEND SELECTION*****`).toEqual('selected')
+    });
+
+    test('Extend selection code scope by using ⌥↑ / Ctrl+W', () => {
+        expect(new ModuleA().test()).toEqual('youpii');
+        expect(`***** REPLACE ME *****`).toEqual('new ModuleA().test()')
+    });
+
+    test('Select next occurrence by using ^G / Alt+J then edit multi lines', () => {
+        let message = `Un tiens vaut mieux que deux tu l'auras.`;
+        message += `Un tiens vaut mieux que deux tu l'auras.`;
+        message += `Un tiens vaut mieux que deux tu l'auras.`;
+        message += `Un tiens vaut mieux que deux tu l'auras.`;
+        message += `Un tiens vaut mieux que deux tu l'auras.`;
+        message += `Un tiens vaut mieux que deux tu l'auras.`;
+        expect(message).toEqual('ha ha ha ha ha ha ');
+    });
+
+    test('Navigate to sayYes definition by using ⌘B / Ctrl+B', () => {
+        expect(new ModuleA().sayYes()).toEqual('yes');
+    });
+
+    test('Delete this test and navigate to to of file by using ⌘A,←', () => {
+        fail('Delete this test and navigate to top');
+    });
+
+
+ */
+
+}
