ee69ff792484a8c12b94301dc9be5c382bcfc365
mathieu cans <mathieucans@gmail.com>
2020-09-25 09:56:49 +0200

    First red test in java

---
+++ java/src/test/java/Koans0BasicsStart.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class Koans0BasicsStart {
+    @Test
+    void myRedTest() {
+        assertThat(true).isFalse();
+    }
+}
