067948c845dc4ca4f9c0fcc5ea6e2453338537ad
mathieu cans <mathieucans@gmail.com>
2020-09-25 10:24:58 +0200

    Translate koans 2

---
+++ java/src/test/java/Koans2CodeGeneration.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Koans2CodeGeneration {
+    @Test
+    void Koans21_uncomment_and_generate_class_and_method() {
+        // by unsing ⌥⏎ | Alt+Enter
+        // TIPS : you can navigate through error code by using F2
+        String buildResult = "code";
+
+        //buildResult = new RobotCode().repeat(buildResult)
+
+        assertThat(buildResult).isEqualTo("codecode");
+    }
+
+    @Test
+    void Koans22_extract_interface_Language_of_French() {
+        // by using ^T | Ctrl+Shift+Alt+T
+
+        // Uncomment this line
+        // const language = English()
+        // Comment this line
+        French language = new French();
+        SayHello sayHello = new SayHello(language);
+
+        String response = sayHello.hello();
+
+        assertThat(response).isEqualTo("Hello David!");
+    }
+
+}
+
+
+class French {
+    public String hello() {
+        return "Salut";
+    }
+}
+
+class SayHello {
+    private French language;
+
+    SayHello(French language) {
+
+        this.language = language;
+    }
+
+    String hello() {
+        return "${this.language.hello()} David!";
+    }
+}
