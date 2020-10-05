5426de3ea6b70fbe971095f719fc304ffe2bbf6e
Mathieu Cans <mathieu.cans@orange.com>
2020-10-05 14:06:43 +0200

    code generation for csharp

--- dotnetcore/Koan02_code_generation.cs
+++ dotnetcore/Koan02_code_generation.cs
@@ -9,16 +9,31 @@ namespace dotnetcore
         void Koan01_uncomment_and_generate_class_and_method() {
             var buildResult = "code";
             // Uncomment following line
-            //buildResult = new RobotCode().repeat(buildResult);
+            // buildResult = new RobotCode().repeat(buildResult);
 
             // Generate class and method by unsing Alt+Enter
-            // TIPS : you can navigate through error code by using F8
+            // TIPS : you can navigate through error code by using Shift+Alt+PgDown
 
             buildResult.Should().Be("codecode");
         }
 
         [Fact]
-        public void Koan01_generate_equality_comparer()
+        public void Koan02_extract_interface_by_using_refactoring_short_cut()
+        {
+            // Extract interface Language of French by using Ctrl+Shift+R
+            // Uncomment this line
+            // var language = new English();
+            // Comment this line
+            var language  = new French();
+            var sayHello = new SayHello(language);
+
+            var response = sayHello.Hello();
+            
+            (response).Should().Be("Hello David!");
+        }
+
+        [Fact]
+        public void Koan03_generate_equality_comparer()
         {
             // Place cursor on class name and use Alt+Insert to generate equality members
             new Person("Pierre", "Dupont").Should().Be(new Person("Pierre", "Dupont"));
@@ -41,20 +56,7 @@ namespace dotnetcore
             }
         }
 
-        [Fact]
-        public void Koan02_extract_interface_by_using_refactoring_short_cut()
-        {
-            // Extract interface Language of French by using Ctrl+Shift+R
-            // Uncomment this line
-            // var language = new English();
-            // Comment this line
-            var language  = new French();
-            var sayHello = new SayHello(language);
-
-            var response = sayHello.Hello();
-            
-            (response).Should().Be("Hello David!");
-        }
+       
     }
 
     public class SayHello
