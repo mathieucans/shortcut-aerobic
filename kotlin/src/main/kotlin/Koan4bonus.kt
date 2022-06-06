221bf9068637d62ea44b20d230f3daab447cca44
mathieu cans <mathieucans@gmail.com>
2022-06-06 20:51:34 +0200

    [kotlin] - add bonus koans

---
+++ kotlin/src/main/kotlin/Koan4bonus.kt
@@ -0,0 +1,8 @@
+class Koan4bonus {
+    fun goToTest(): String {
+        // by using ⌘⇧T / Ctrl+Shift+T
+        return "remove me, uncomment the following line and go back to test"
+        // return "go to test";
+    }
+
+}
---
+++ kotlin/src/test/kotlin/Koan4BonusTest.kt
@@ -0,0 +1,31 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Koan4BonusTest {
+    @Test
+    fun `01 - cut up to line end`() {
+        val lines = ("keep " + "--- delete me with ^K"
+                + "the " + "--- this short couts"
+                + "firsts " + "--- works in terminal too"
+                + "words")
+        assertThat(lines).isEqualTo("keep the firsts words")
+    }
+
+    @Test
+    fun `02 - past from history`() {
+        // by using ⌘⇧V / Ctrl+Shift+V
+        assertThat("past here an old text you have copy").isEqualTo(" + \"--- delete me with ^K\"")
+    }
+
+    @Test
+    fun `03 - got to same named test`() {
+        // by using ⌘⇧T / Ctrl+Shift+T
+        assertThat(Koan4bonus().goToTest()).isEqualTo("go to test")
+    }
+
+    @Test
+    fun `04 - run anything`() {
+        // by using Double ^ / Ctrl, Ctrl
+        assertThat("then type gradle test").isEqualTo("all green")
+    }
+}
