17e634104c1712e2b24880e85e9321f7f571cc43
mathieu cans <mathieucans@gmail.com>
2022-06-06 18:52:34 +0200

    [typescript] - add past from history and got to test

---
+++ typescript/src/koan.4.bonus.ts
@@ -0,0 +1,7 @@
+export class Koan4Bonus {
+    goToTest() {
+        // by using ⌘⇧T / Ctrl+Shift+T
+        throw 'remove me and go back to test'
+        return 'go to test'
+    }
+}
--- typescript/test/koan.4.bonus.test.ts
+++ typescript/test/koan.4.bonus.test.ts
@@ -1,3 +1,5 @@
+import {Koan4Bonus} from "../src/koan.4.bonus";
+
 describe('some bonus shortkut', () => {
     test('cut up to line end', () => {
         const lines = `keep --- delete me with ^K
@@ -10,5 +12,15 @@ the
 firsts
 words
  `)
+    });
+
+    test('past from history', () => {
+        // by using ⌘⇧V / Ctrl+Shift+V
+        expect('past here an old text you have copy').toEqual(' --- works')
+    });
+
+    test('got to same named test', () => {
+        // by using ⌘⇧T / Ctrl+Shift+T
+        expect(new Koan4Bonus().goToTest()).toEqual('go to test')
     });
 });
