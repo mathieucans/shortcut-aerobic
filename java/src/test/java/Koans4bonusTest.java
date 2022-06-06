62d023faf8ab995230a656cc146e8b5b000f5bb6
mathieu cans <mathieucans@gmail.com>
2022-06-06 20:05:27 +0200

    [java] - add cut up to line end

---
+++ java/src/test/java/Koans4bonusTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Koans4bonusTest {
+    @Test
+    public void cut_up_to_line_end() {
+        String lines = "keep " + "--- delete me with ^K"
+        + "the " + "--- this short couts"
+        + "firsts " + "--- works in terminal too"
+        + "words";
+
+        assertThat(lines).isEqualTo("keep the firsts words");
+    }
+}
