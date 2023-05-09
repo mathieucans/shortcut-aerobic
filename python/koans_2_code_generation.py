600c46e59fddea4d49885c4c48d5de8f3b1d67f1
mathieu cans <mathieu.cans@orange.com>
2023-05-09 09:41:11 +0200

    [python] add koan : extract_interface_Language_from_French

--- python/koans_2_code_generation.py
+++ python/koans_2_code_generation.py
@@ -1,53 +1,48 @@
 from unittest import TestCase
 
+
 # Generate code by using your IDE
 class Koans2CodeGeneration(TestCase):
     def testKoan01_uncomment_and_generate_class_and_method(self):
         # by using ⌥⏎ | Alt+Enter
-        #TIPS : you can navigate through error code by using F2
-        buildResult = "code"
+        # TIPS : you can navigate through error code by using F2
+        build_result = "code"
 
-        #buildResult = new RobotCode().repeat(buildResult);
+        # build_result = new RobotCode().repeat(buildResult);
 
-        self.assertEqual(buildResult,"codecode")
-
+        self.assertEqual(build_result, "codecode")
 
     def testKoan02_surround_with_try_except(self):
         # Surround the following line with try except by using ⌥⌘T | Ctrl+Alt+T
         raise Exception('surround me with try except')
 
-#
-# test('03 - extract interface Language from French', () => {
-# // Extract interface Language of French and generate English langage.
-# // Then change SayHello parameter and field type to speak english.
-# // Tips : To extract interface, display the refactoring menu by using ^T | Ctrl+Shift+Alt+T
-# // Check the methods you want to have in your interface and make English implement it by using ⌥⏎ | Alt+Enter
-#
-# // const language = new English();      // Uncomment this line
-# const language = new French();          // Comment this line
-#
-# let sayHello = new SayHello(language);
-#
-# let response = sayHello.hello();
-#
-# expect(response).toEqual('Hello David!');
-# });
-# });
-#
-# // Extract language interface
-# class French {
-# hello() {
-# return 'Salut';
-# }
-# }
-#
-# class SayHello {
-# constructor(private readonly language: French) {
-#
-# }
-#
-# hello() {
-# return `${this.language.hello()} David!`;
-# }
-# }
-#
+    def testKoan03_extract_abstract_class_Language_from_French(self):
+        # Extract abstract class Language of French and generate English langage.
+        # Then change SayHello parameter and field type to speak english.
+        # Tips : To extract superclass, display the refactoring menu by using ^T | Ctrl+Shift+Alt+T
+        # Check the methods you want to have in your abstract class and make English implement it by using ⌥⏎ | Alt+Enter
+
+        # language = English()     # Uncomment this line
+        language = French()  # Comment this line
+
+        say_hello = SayHello(language)
+
+        response = say_hello.hello()
+
+        self.assertEqual(response, "Hello David!")
+
+
+# Extract language interface from language
+class French:
+    def hello(self):
+        return 'Salut'
+
+
+class SayHello:
+    language: French
+
+    def __init__(self, language: French):
+        self.language = language
+
+    def hello(self):
+        return self.language.hello() + " David!"
